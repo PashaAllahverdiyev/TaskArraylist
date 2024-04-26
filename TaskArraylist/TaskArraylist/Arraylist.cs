a669887ff7953b99fc63f1a2983873f8478c538b
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-04-26 09:00:31 +0400

    Add files via upload

---
+++ TaskArraylist/TaskArraylist/Arraylist.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace TaskArraylist
+{
+    public class Arraylist : IEnumerable
+    {
+        public object[] _arr = new object[0];
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+        public Arraylist()
+        {
+            Capacity = 4;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in _arr)
+            {
+                yield return item;
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            if(index>0 || index <= Count)
+            {
+                for(int i=0; i<_arr.Length-1; i++)
+                {
+                    _arr[i] = _arr[i+1];
+                }
+                Array.Resize(ref _arr, _arr.Length-1);
+                Count--;
+                if (Capacity / 2 >= Count) Capacity /= 2;
+            }
+
+        }
+        public void Remove(object value)
+        {
+            int remove = Array.IndexOf(_arr, value);
+            RemoveAt(remove);
+        }
+        public void Add(object value)
+        {
+            Array.Resize(ref _arr, _arr.Length + 1);
+            _arr[_arr.Length - 1] = value;
+            Count++;
+            if (Capacity < Count) Capacity *= 2;
+        }
+        public void TrimToSize()
+        {
+            if (Capacity > Count)
+                Array.Resize(ref _arr, Capacity = Count);
+        }
+        public void AddRange(ICollection collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+    }   
+}
---
+++ TaskArraylist/TaskArraylist/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TaskArraylist;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Arraylist arraylist = new Arraylist()
+           {
+            "Pasha",
+            "Ali",
+            "Hello",
+            24,
+            23,
+            20
+            
+           };
+        arraylist.Remove("Hello");
+        arraylist.Remove(20);
+        arraylist.TrimToSize();
+        arraylist.Add("Car");
+        Console.WriteLine(arraylist._arr.Length);
+        Console.WriteLine(arraylist.Count);
+    }
+}
